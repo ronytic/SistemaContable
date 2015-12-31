380a382de5535c942b7e3a520ec033a9ae7d087a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-31 11:45:36 -0400

    Cabecerahtml

---
+++ cabecerahtml.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Sistema Contable - SISCON</title>
+
+    <link href="<?php echo $folder?>css/core/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo $folder?>css/core/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="<?php echo $folder?>css/core/animate.css" rel="stylesheet">
+    <link href="<?php echo $folder?>css/core/style.css" rel="stylesheet">

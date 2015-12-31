223153b2f940ee79eec93090da336aecc8edff72
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-31 16:12:26 -0400

    Logout

--- login/logout.php
+++ login/logout.php
@@ -6,11 +6,11 @@ foreach($_SESSION as $k=>$v){
 //	echo $k."-".$v;
 	unset($_SESSION[$k]);
 }
-unset($_SESSION["login"]);
+unset($_SESSION["Login"]);
 unset($_SESSION["CodUsuarioLog"]);
-unset($_SESSION["CodAl"]);
 unset($_SESSION["Nivel"]);
-unset($_SESSION["CodDocente"]);
+unset($_SESSION["LoginSistemaContable"]);
+
 session_destroy();
 header("Location:".url_base().$directory);
 ?>

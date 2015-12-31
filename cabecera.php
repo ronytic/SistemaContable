0cfb8fd2d6690150b8808147adfe11f128e81e2f
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-31 12:52:59 -0400

    Cabecera

--- cabecera.php
+++ cabecera.php
@@ -12,7 +12,7 @@
                             <img alt="image" class="img-circle" src="<?php echo $folder?>imagenes/usuarios/general.jpg" />
                              </span>
                         <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
+                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David WilliamsNiñoí</strong>
                              </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
                         <ul class="dropdown-menu animated fadeInRight m-t-xs">
                             <li><a href="profile.html">Profile</a></li>

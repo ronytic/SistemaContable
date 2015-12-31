4c88f382fe218c1bd745f94b0d8ef61bad328bb9
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-31 16:12:11 -0400

    Login Check

--- login/check.php
+++ login/check.php
@@ -4,13 +4,13 @@ $dir=dirname(__FILE__).DIRECTORY_SEPARATOR."../";
 define("RAIZ",$dir);
 include_once(RAIZ."configuracion.php");
 include_once(RAIZ."rastreo/revisar.php");
-if(!(isset($_SESSION["LoginSistemaColegio"]) && $_SESSION['LoginSistemaColegio']==1)){
+if(!(isset($_SESSION["LoginSistemaContable"]) && $_SESSION['LoginSistemaContable']==1)){
 	include_once(RAIZ."funciones/url.php");
 	header("Location:".url_base().$directory."login/?u=".$_SERVER['PHP_SELF']);
 }else{
-	$idiomaarchivo=$_SESSION['Idioma']!=""?$_SESSION['Idioma']:"es";
-	if(!file_exists(RAIZ."idioma/".$idiomaarchivo.".php")){$idiomaarchivo="es";}
-	include_once(RAIZ."idioma/".$idiomaarchivo.".php");	
+	//$idiomaarchivo=$_SESSION['Idioma']!=""?$_SESSION['Idioma']:"es";
+	//if(!file_exists(RAIZ."idioma/".$idiomaarchivo.".php")){$idiomaarchivo="es";}
+	//include_once(RAIZ."idioma/".$idiomaarchivo.".php");	
 	include_once(RAIZ."funciones/funciones.php");	
 }
 ?>

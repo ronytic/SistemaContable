6ef9d283844d29a98de28aa069686ce2045f1f27
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-31 18:47:58 -0400

    Pie

--- pie.php
+++ pie.php
@@ -10,15 +10,19 @@
 
         </div>
         </div>
-
-    <!-- Mainly scripts -->
+    <script language="javascript">
+        var folder="<?php echo $folder?>";
+    </script>
+    <!-- Principal scripts -->
     <script src="<?php echo $folder?>js/core/jquery-2.1.1.js"></script>
     <script src="<?php echo $folder?>js/core/bootstrap.min.js"></script>
     <script src="<?php echo $folder?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="<?php echo $folder?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
-    <!-- Custom and plugin javascript -->
+    <!-- Plugins Modificados Javascript -->
     <script src="<?php echo $folder?>js/core/inspinia.js"></script>
+    <script src="<?php echo $folder?>js/plugins/form/jquery.form.js"></script>
+    <script src="<?php echo $folder?>js/plugins/busqueda/busqueda.js"></script>
     <script src="<?php echo $folder?>js/plugins/pace/pace.min.js"></script>
 
 

689733ca1ded71c2651c3c486da9792fd6e866b9
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-31 11:45:40 -0400

    Pie

---
+++ pie.php
@@ -0,0 +1,27 @@
+</div>
+            <div class="footer">
+                <div class="pull-right">
+                    10GB of <strong>250GB</strong> Free.
+                </div>
+                <div>
+                    <strong>Copyright</strong> Example Company &copy; 2014-2015
+                </div>
+            </div>
+
+        </div>
+        </div>
+
+    <!-- Mainly scripts -->
+    <script src="<?php echo $folder?>js/core/jquery-2.1.1.js"></script>
+    <script src="<?php echo $folder?>js/core/bootstrap.min.js"></script>
+    <script src="<?php echo $folder?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="<?php echo $folder?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="<?php echo $folder?>js/core/inspinia.js"></script>
+    <script src="<?php echo $folder?>js/plugins/pace/pace.min.js"></script>
+
+
+</body>
+
+</html>

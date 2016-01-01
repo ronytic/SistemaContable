ae6c86485fc2e8dc8c60b360be615624bb9d026d
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-31 20:27:53 -0400

    Eliminar Guardar

---
+++ plandecuentas/capitulos/guardar.php
@@ -0,0 +1,23 @@
+<?php
+include("../../login/check.php");
+extract($_POST);
+include("../../class/plancuentas_capitulo.php");
+$plancuentas_capitulo=new plancuentas_capitulo;
+$valores=array("Nombre"=>"'$Nombre'");
+$plancuentas_capitulo->insertarRegistro($valores);
+$titulo="Plan de Cuentas - Capítulos";
+$folder="../../";
+?>
+<?php include_once($folder."cabecerahtml.php");?>
+<?php include_once($folder."cabecera.php");?>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox-content text-center p-md">
+                <h2><span class="text-navy">DATOS GUARDADOS CORRECTAMENTE</span></h2>
+                <a href="./" class="btn btn-success">Volver</a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once($folder."pie.php");?>

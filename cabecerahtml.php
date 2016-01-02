d31728119d21d3cb7a9bbdd6815e39854e58aa79
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-01-02 11:45:39 -0400

    Cabecera

--- cabecerahtml.php
+++ cabecerahtml.php
@@ -28,5 +28,7 @@ $submenu=new submenu;
     <link href="<?php echo $folder?>css/core/animate.css" rel="stylesheet">
     <link href="<?php echo $folder?>css/core/style.css" rel="stylesheet">
     <link href="<?php echo $folder?>css/core/core.css" rel="stylesheet">
+    <link href="<?php echo $folder?>css/core/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css" rel="stylesheet">
+
     
     <script src="<?php echo $folder?>js/core/jquery-2.1.1.js"></script>

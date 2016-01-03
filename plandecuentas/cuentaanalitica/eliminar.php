3a02609738a409d22f5efb0602175ff18adf2d31
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-01-03 16:03:08 -0400

    Eliminado

--- plandecuentas/cuentaanalitica/eliminar.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../../login/check.php");
-extract($_POST);
-include("../../class/plancuentas_subcuenta.php");
-$plancuentas_subcuenta=new plancuentas_subcuenta;
-$plancuentas_subcuenta->eliminarRegistro("CodSubcuenta=$Cod");
-
-?>

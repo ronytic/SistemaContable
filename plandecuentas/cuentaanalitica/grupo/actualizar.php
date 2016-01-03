b67338ba30bce69fe79f62996146f330a7ddeaae
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-01-03 17:15:24 -0400

    Guardar Actualizar

---
+++ plandecuentas/cuentaanalitica/grupo/actualizar.php
@@ -0,0 +1,8 @@
+<?php
+include("../../../login/check.php");
+extract($_POST);
+include("../../../class/plancuentas_grupo.php");
+$plancuentas_grupo=new plancuentas_grupo;
+$valores=array("Codigo"=>"'$Codigo'","Nombre"=>"'$Nombre'","CodCapitulo"=>"'$CodCapitulo'");
+$plancuentas_grupo->actualizarRegistro($valores,"CodGrupo=$Cod");
+

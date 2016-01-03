e19353c120f8272f59b9af9042f81fdb2bd013bb
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-01-03 16:03:13 -0400

    INicio

--- plandecuentas/cuentaanalitica/index.php
+++ plandecuentas/cuentaanalitica/index.php
@@ -13,9 +13,6 @@ $(document).on("ready",function(){
     //listarcuentaanalitica();
     $(document).on("change","[name=CodCapitulo]",function(){
         listargrupos(); 
-        $("#listadocuentas").html("");   
-        $("#listadosubcuentas").html("");
-        $("#listadocuentaanalitica").html("");
     });
     $(document).on("change","[name=CodGrupo]",function(){
         listarcuentas();   
@@ -26,8 +23,55 @@ $(document).on("ready",function(){
     $(document).on("change","[name=CodSubcuenta]",function(){
         listarcuentaanalitica();   
     });
+    $(document).on("click","#nuevocapitulo",function(e){
+        e.preventDefault();
+        $.post("capitulo/formulario_capitulo.php",{},function(data){
+            $("#formulariocapitulo").html(data).slideDown("slow");    
+        });
+    });
+    $(document).on("click"," .cerrar",function(e){
+        e.preventDefault();
+        $(this).parent().parent().parent().html('');
+    });
+    $(document).on("click","#GuardarCapitulo",function(e){
+        e.preventDefault();
+        var Codigo=$("#formulariocapitulo [name=Codigo]").val(); 
+        var Nombre=$("#formulariocapitulo [name=Nombre]").val(); 
+        var Cod=$("#formulariocapitulo [name=Cod]").val(); 
+        var ArchivoGuardar=$("#formulariocapitulo [name=ArchivoGuardar]").val(); 
+        $.post(ArchivoGuardar,{"Codigo":Codigo,"Nombre":Nombre,"Cod":Cod},function(){
+            listarcapitulos();    
+             $("#formulariocapitulo").html('');
+        })
+    })
+    $(document).on("click",".modificarcuenta",function(e){
+        e.preventDefault();
+        if(confirm("¿Esta seguro que desea Modificar esta Cuenta?")){
+            var dir=$(this).attr("href");
+            var formulario=$(this).attr("rel-formulario");
+            $.get(dir,{},function(data){
+               $("#"+formulario).html(data);
+            }); 
+        }
+        
+    })
+    $(document).on("click",".eliminarcuenta",function(e){
+        e.preventDefault();
+        if(confirm("¿Esta seguro que desea Eliminar esta Cuenta?\nRecuerde que si contiene algun elemento dentro su jerarquia se perderan sin posibilidad de recuperarlo")){
+            var dir=$(this).attr("href");
+            var listado=$(this).attr("rel-cuenta");
+            $.get(dir,{},function(data){
+               setTimeout(listado+"()",0)
+            });
+        }
+        
+    })
 });
 function listarcapitulos(){
+    $("#listadogrupos").html(""); 
+    $("#listadocuentas").html("");   
+    $("#listadosubcuentas").html("");
+    $("#listadocuentaanalitica").html("");
     $.post("mostrar_capitulos.php",{},function(data){
         $("#listadocapitulos").html(data);
     });
@@ -62,7 +106,10 @@ function listarcuentaanalitica(){
     <div class="col-lg-4">
         <div class="ibox">
             <div class="ibox-title"><h5>1.- Capítulo </h5> <span class="pull-right"><a href="#" id="nuevocapitulo" class="btn btn-xs btn-success"><i class="fa fa-plus"></i></a></span></div>
-            <div class="ibox-content" id="listadocapitulos"></div>
+            <div class="ibox-content">
+                <div id="listadocapitulos"></div>
+                <div id="formulariocapitulo"></div>
+            </div>
         </div>
     </div><!--Col12-->
     <div class="col-lg-4">

f17da3bc86b9220cb50ac9695aa6b891d0f28382
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-01-03 16:03:48 -0400

    Formulario Capitulo

---
+++ plandecuentas/cuentaanalitica/capitulo/formulario_capitulo.php
@@ -0,0 +1,30 @@
+<?php
+include_once("../../../login/check.php");
+if($_GET['Cod']!=""){
+    $titulo="Modificar";  
+    $archivo="actualizar.php";
+    include_once("../../../class/plancuentas_capitulo.php");
+    $plancuentas_capitulo=new plancuentas_capitulo;
+    $pc_cap=$plancuentas_capitulo->mostrarTodoRegistro("CodCapitulo=".$_GET['Cod'],"1");
+    $pc_cap=array_shift($pc_cap);
+    
+}else{
+    $titulo="Nuevo";    
+    $archivo="guardar.php";
+}
+?>
+<small>
+    <strong><?php echo $titulo?></strong>
+    <span class="pull-right">
+        <a href="#" class="cerrar"><i class="fa fa-close"></i></a>
+    </span>
+</small>
+<div class="input-group">
+    <input type="text" name="Codigo" value="<?php echo $pc_cap['Codigo']?>" placeholder="Código" class="input input-sm  form-control col-sm-2" size="2" style="width:25%;">
+    <input type="text" name="Nombre" value="<?php echo $pc_cap['Nombre']?>" placeholder="Nombre" class="input input-sm form-control col-sm-12" size="20" style="width:75%;"> 
+    <span class="input-group-btn">
+        <a href="#" class="btn btn-sm btn-success" id="GuardarCapitulo">Guardar</a>
+    </span>
+</div>
+<input type="text" value="<?php echo $_GET['Cod']?>" name="Cod">
+<input type="text" value="capitulo/<?php echo $archivo?>" name="ArchivoGuardar">

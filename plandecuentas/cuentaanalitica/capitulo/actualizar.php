8bd81d2a15c1e82d685009fb2f21e364fcb05351
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-01-03 16:03:31 -0400

    Actualizar Capitulo

---
+++ plandecuentas/cuentaanalitica/capitulo/actualizar.php
@@ -0,0 +1,8 @@
+<?php
+include("../../../login/check.php");
+extract($_POST);
+include("../../../class/plancuentas_capitulo.php");
+$plancuentas_capitulo=new plancuentas_capitulo;
+$valores=array("Codigo"=>"'$Codigo'","Nombre"=>"'$Nombre'");
+$plancuentas_capitulo->actualizarRegistro($valores,"CodCapitulo=$Cod");
+

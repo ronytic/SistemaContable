f81dee71f8a7885100e498eb0e8011b0c3c206a8
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-01-03 22:03:49 -0400

    Cuenta Analitica Guardar

---
+++ plandecuentas/cuentaanalitica/cuentaanalitica/guardar.php
@@ -0,0 +1,8 @@
+<?php
+include("../../../login/check.php");
+extract($_POST);
+include("../../../class/plancuentas_cuentaanalitica.php");
+$plancuentas_cuentaanalitica=new plancuentas_cuentaanalitica;
+$valores=array("Codigo"=>"'$Codigo'","Nombre"=>"'$Nombre'","CodCapitulo"=>"'$CodCapitulo'","CodGrupo"=>"'$CodGrupo'","CodCuenta"=>"'$CodCuenta'","CodSubcuenta"=>"'$CodSubcuenta'");
+$plancuentas_cuentaanalitica->insertarRegistro($valores);
+?>

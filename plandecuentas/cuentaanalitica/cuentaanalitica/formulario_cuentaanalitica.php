9b983ae3032b59c9a4460a6928f8cba09a12fe3a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-01-04 12:50:21 -0400

    Formulario Cuenta Analitica

--- plandecuentas/cuentaanalitica/cuentaanalitica/formulario_cuentaanalitica.php
+++ plandecuentas/cuentaanalitica/cuentaanalitica/formulario_cuentaanalitica.php
@@ -11,6 +11,13 @@ if($_GET['Cod']!=""){
 }else{
     $titulo="Nuevo";    
     $archivo="guardar.php";
+    
+    include_once("../../../class/plancuentas_cuentaanalitica.php");
+    $plancuentas_cuentaanalitica=new plancuentas_cuentaanalitica;
+    $plancuentas_cuentaanalitica->campos=array("MAX(Codigo) as Codigo");
+    $pc_cap=$plancuentas_cuentaanalitica->mostrarTodoRegistro("CodSubcuenta=".$_POST['CodSubcuenta'],1,"Codigo");
+    $pc_cap=array_shift($pc_cap);
+    $pc_cap['Codigo']+=1;
 }
 ?>
 <small>
@@ -20,8 +27,9 @@ if($_GET['Cod']!=""){
     </span>
 </small>
 <div class="input-group">
-    <input type="text" name="Codigo" value="<?php echo $pc_cap['Codigo']?>" placeholder="Código" class="input input-sm  form-control col-sm-2" size="2" style="width:25%;">
-    <input type="text" name="Nombre" value="<?php echo $pc_cap['Nombre']?>" placeholder="Nombre" class="input input-sm form-control col-sm-12" size="20" style="width:75%;"> 
+    <input type="text" name="Codigo" value="<?php echo $pc_cap['Codigo']?>" placeholder="Código" class="input input-sm  form-control col-sm-2" size="2" style="width:15%;">
+    <input type="text" name="Nombre" value="<?php echo $pc_cap['Nombre']?>" placeholder="Nombre" class="input input-sm form-control col-sm-12" size="20" style="width:50%;"> 
+    <input type="text" name="Detalle" value="<?php echo $pc_cap['Detalle']?>" placeholder="Detalle" class="input input-sm form-control col-sm-12" size="20" style="width:35%;"> 
     <span class="input-group-btn">
         <a href="#" class="btn btn-sm btn-success" id="GuardarCuentaAnalitica">Guardar</a>
     </span>
